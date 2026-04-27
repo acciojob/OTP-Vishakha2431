921cd847835412828cf0588a7556b447146547d8
Vishakha Gautam <156678713+Vishakha2431@users.noreply.github.com>
2026-04-27 21:47:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-//your JS code here. If required.
 let inputs = document.querySelectorAll(".code");
 
-// FORWARD MOVEMENT
+
+inputs[0].focus();
+
 inputs.forEach((input, index) => {
-  input.addEventListener("input", () => {
-    if (input.value.length === 1 && index < inputs.length - 1) {
+
+  input.addEventListener("input", (e) => {
+    // allow only 1 digit
+    input.value = input.value.slice(0, 1);
+
+    if (input.value && index < inputs.length - 1) {
       inputs[index + 1].focus();
     }
   });
 
-  
   input.addEventListener("keydown", (e) => {
-    if (e.key === "Backspace"  && index > 0) {
-      inputs[index - 1].focus();
+    if (e.key === "Backspace") {
+
+      if (input.value === "" && index > 0) {
+        inputs[index - 1].focus();
+      }
     }
   });
 });
